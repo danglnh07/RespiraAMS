490fe22b252706ccbba559bbaca40b0768389e32
Le Nguyen Hai Dang <danglnh07.work@gmail.com>
2026-05-27 16:52:22 +0700

    Add migration with new model changes

---
+++ backend/src/Infrastructure/RespiraAMS.Infrastructure/Migrations/20260527095050_UpdateSpectrumModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RespiraAMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSpectrumModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "antibiotic_spectra",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "antibiotic_spectra");
+        }
+    }
+}
